9253b5b0876df4e7f3d55e722f8c1ecf994d29b9
Alex <aeb0975@gmail.com>
2025-01-17 15:34:16 -0500

    change of sign

--- contracts/CrowdSource.sol
+++ contracts/CrowdSource.sol
@@ -6,7 +6,6 @@
 
 pragma solidity ^0.8.18;
 
-
 import {PriceConverter} from "./PriceConverter.sol";
 
 error NotOwner();
@@ -14,49 +13,52 @@ error TransactionFailed();
 error NotEnoughMoney();
 
 contract CrowdSource {
-
     using PriceConverter for uint256;
 
     uint256 public constant MINIMUM_USD = 5e18;
 
     address[] public funders;
 
-    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    mapping(address funder => uint256 amountFunded)
+        public addressToAmountFunded;
 
     address public immutable i_owner;
 
     constructor() {
         i_owner = msg.sender;
-    } 
+    }
 
-
-    function fund() public payable{
-        if (msg.value.getConversionRate() >= MINIMUM_USD) { // 1e18 = 1 ETH
+    function fund() public payable {
+        if (msg.value.getConversionRate() < MINIMUM_USD) {
+            // 1e18 = 1 ETH
             revert NotEnoughMoney();
         }
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] += msg.value;
     }
 
-   
-
-    function withdraw() public onlyOwner{
-
-        for (uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+    function withdraw() public onlyOwner {
+        for (
+            uint256 funderIndex = 0;
+            funderIndex < funders.length;
+            funderIndex++
+        ) {
             address funder = funders[funderIndex];
             addressToAmountFunded[funder] = 0;
         }
 
         funders = new address[](0);
 
-        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        (bool callSuccess, ) = payable(msg.sender).call{
+            value: address(this).balance
+        }("");
         if (!callSuccess) {
             revert TransactionFailed();
         }
     }
 
     modifier onlyOwner() {
-        if(msg.sender != i_owner) {
+        if (msg.sender != i_owner) {
             revert NotOwner();
         }
         _; // the underscore tells the function which contains the modifier in its declaration to execute the require then anything else that is in the function
